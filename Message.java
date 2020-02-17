2661979eec83e2bae2ad6e8a1e123d176cce5126
glebka191 <60113335+glebka191@users.noreply.github.com>
2020-02-17 20:08:51 +0300

    Update Message.java

--- Message.java
+++ Message.java
@@ -5,24 +5,24 @@ package com.javarush.task.task30.task3008;
 import java.io.Serializable;
 
 public class Message implements Serializable {
-    private final MessageType type;
-    private final String data;
+    private final MessageType type;         //тип сообщения//
+    private final String data;              //данные сообщения//
 
-    public Message(MessageType type) {
+    public Message(MessageType type) {      //Конструктор принимающий тип сообщения, инициализирующий data из-за final//
         this.type = type;
         this.data = null;
     }
 
-    public Message(MessageType type, String data) {
+    public Message(MessageType type, String data) {     // 2-й конструктор//
         this.type = type;
         this.data = data;
     }
 
-    public MessageType getType() {
+    public MessageType getType() {                  //get//
         return type;
     }
 
-    public String getData() {
+    public String getData() {                       //get//
         return data;
     }
 }
