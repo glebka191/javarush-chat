7eb0f14d79e9023b032cabe541626072a2dc37e6
glebka191 <60113335+glebka191@users.noreply.github.com>
2020-02-17 20:22:12 +0300

    Update Connection.java

--- Connection.java
+++ Connection.java
@@ -1,4 +1,10 @@
-//класс соединения между клиентом и сервером.//
+//класс соединения между клиентом и сервером.
+//Клиент и сервер будут общаться через сокетное соединение.
+//Одна сторона будет записывать данные в сокет, а другая читать.
+//Их общение представляет собой обмен сообщениями Message.
+//Класс Connection будет выполнять роль обертки над классом java.net.Socket,
+//которая должна будет уметь сериализовать и десериализовать объекты типа Message в сокет.
+//Методы этого класса должны быть готовы к вызову из разных потоков
 
 package com.javarush.task.task30.task3008;
 
@@ -11,8 +17,8 @@ import java.net.Socket;
 import java.net.SocketAddress;
 
 public class Connection implements Closeable {
-    public Connection(Socket socket) throws IOException {
-        this.socket = socket;
+    public Connection(Socket socket) throws IOException {       //Конструктор, который принимает Socket в качестве параметра
+        this.socket = socket;                                   //и инициализирует поля.
         this.out = new ObjectOutputStream(socket.getOutputStream());
         this.in = new ObjectInputStream(socket.getInputStream());
     }
@@ -23,25 +29,25 @@ public class Connection implements Closeable {
 
 
     @Override
-    public void close() throws IOException {
+    public void close() throws IOException {        //переопределил метод и закрываем все ресурсы нашего класса
         out.close();
         in.close();
         socket.close();
     }
 
-    public void send(Message message) throws IOException{
-        synchronized(out) {
+    public void send(Message message) throws IOException{   //записываем сообщение message в ObjectOutputStream
+        synchronized(out) {                                 //и синхронизируем т.к. доступ будет из разных потоков
             out.writeObject(message);
         }
     }
 
-    public Message receive() throws IOException, ClassNotFoundException{
+    public Message receive() throws IOException, ClassNotFoundException{    //тоже самое только читаем
         synchronized(in) {
             return (Message) in.readObject();
         }
     }
 
-    public SocketAddress getRemoteSocketAddress(){
+    public SocketAddress getRemoteSocketAddress(){             //возвращает удаленный адрес сокетного соединения
         return socket.getRemoteSocketAddress();
     }
 }
