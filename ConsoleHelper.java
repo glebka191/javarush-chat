49144f7be8b7a6d39b1b95d080eb7a7b5ec362bf
glebka191 <60113335+glebka191@users.noreply.github.com>
2020-02-17 19:58:42 +0300

    Update ConsoleHelper.java

--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -10,12 +10,14 @@ import java.io.InputStreamReader;
 
 public class ConsoleHelper {
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+    
+    //выводим message В консоль//
     public static void writeMessage(String message){
         System.out.println(message);
 
     }
-
+    
+    //читаем строку с консоли, если исключение вывести текст ошибки, повторить ввод//
     public static String readString() {
         Boolean OK = false;
         String strret = null;
@@ -30,12 +32,13 @@ public class ConsoleHelper {
         return strret;
     }
 
+    //парсим число используя метод readString чтения со строки//
     public static int readInt(){
         try {
             return Integer.parseInt(readString());
         } catch (NumberFormatException e) {
             System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
-            return Integer.parseInt(readString());//Как повторить ввод?
+            return Integer.parseInt(readString());
         }
     }
 
